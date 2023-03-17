101b3d4aafc9f5a7c6fc4bfda171bcf16e78901c
MichalKulma <michal.kulma1502@gmail.com>
2023-03-17 16:22:12 +0100

    a

--- klient/app.js
+++ klient/app.js
@@ -1,55 +1,29 @@
-async function getdata(){
-    const data=await fetch("http://localhost:3000/dane")
-    json=await data.json()
+async function getuser(){
+const login=document.getElementById("login").value
+const haslo=document.getElementById("haslo").value
 
+const data=await fetch(`http://localhost:3000/get/${login}/${haslo}`)
+const json=await data.json()
+console.log(json)
+
+if(json.user!=undefined){
+    localStorage.setItem("upr",JSON.stringify(json))
 }
-
-
-function checklogin(){
-    const login=document.getElementById("login").value
-    const haslo=document.getElementById("haslo").value
-
-    const user="admin"
-    const adminpass="admin"
-const user1="user"
-const userpass="user"
-
-    if(login==user && haslo==adminpass){
-        console.log("zalogowano")
-        localStorage.setItem("login","admin")
-    window.location.href="admin.html"
-    }
-
-    else if(login==user1 && haslo==userpass){
-        console.log("zalogowano")
-        localStorage.setItem("login","user")
-        window.location.href="user.html"
-    }
-    else{ console.log("błąd")
-alert("błąd")
-localStorage.setItem("login","false")
-}
-}
-
-
-function checkadmin(){
-const admin=localStorage.getItem("login")
-
-if(admin!="admin"){
-    window.location.href="login.html"
-}
-}
-
-function logout(){
-    localStorage.setItem("login","false")
+else localStorage.setItem("upr","false")
 }
 
 function checkuser(){
-    const user=localStorage.getItem('login')
-    if(user!="user" ){
-        window.location.href="login.html"
-    }
-    else if(user!="admin"){
-        window.location.href="login.html"
-    }
+const user=JSON.parse(localStorage.getItem("upr"))
+const url=window.location.href
+if(user.upr!="admin" && url.includes("admin.html")){
+window.location.href="index.html"
+}
+
+
+}
+
+function logout(){
+    const user=localStorage.getItem("upr")
+
+    localStorage.setItem(user,"false")
 }
--- serwer/index.js
+++ serwer/index.js
@@ -2,14 +2,30 @@ const express=require('express')
 const cors=require('cors')
 const app=express()
 app.use(cors())
+const port=3000
+
+const users=[
+    {user:"admin",haslo:"admin",upr:"admin"},
+    {user:"user",haslo:"user",upr:"user"},
+    {user:"Jan",haslo:"Kowalski",upr:"user"}
+]
+
+app.get('/get/:user/:haslo',(req,res)=>{
+const user=req.params.user
+const haslo=req.params.haslo
+
+for(let i=0;i<=users.length-1;i++){
+if(users[i].user==user && users[i].haslo==haslo){
+res.json({user:user,upr:users[i].upr})
+}
 
 
-
-app.get('/dane',function(req,res){
-
+}
 
 })
 
 
 
-app.listen(3000)
+app.listen(port, ()=> {
+    console.log("aplikacja dziala na porcie "+port)
+})
